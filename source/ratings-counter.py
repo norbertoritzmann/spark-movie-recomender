396374d466436c132c952ae2731156a8668770e2
norbertoritzmann <norberto.ritzmann@gmail.com>
2017-04-06 16:42:52 -0300

    A path change

--- source/ratings-counter.py
+++ source/ratings-counter.py
@@ -4,7 +4,7 @@ import collections
 conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
 sc = SparkContext(conf = conf)
 
-lines = sc.textFile("../ml-100k/u.data")
+lines = sc.textFile("../../ml-100k/u.data")
 ratings = lines.map(lambda x: x.split()[2])
 result = ratings.countByValue()
 
